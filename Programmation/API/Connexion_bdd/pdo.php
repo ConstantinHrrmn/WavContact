a277eb913160f7dbd58a03fb66a49ce4b4d6d05a
ConstantinHrrmn <31920335+ConstantinHrrmn@users.noreply.github.com>
2021-11-20 21:07:29 +0100

    création du login

---
+++ Programmation/API/Connexion_bdd/pdo.php
@@ -0,0 +1,25 @@
+<?php
+//error_reporting(E_ALL);
+session_start();
+
+//Constantes
+define("DB_HOST", "localhost");
+define("DB_NAME", "WavContact");
+define("DB_USER", "waview");
+define("DB_PASSWORD", "Waview2019_!");
+
+function database(){
+    static $dbc = null;
+
+    if ($dbc == null) {
+        try{
+            $dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD,
+                    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::ATTR_PERSISTENT => TRUE));
+        } catch (Exception $e) {
+            echo 'Erreur : ' . $e->getMessage() . '<br/>';
+            echo 'N° : ' . $e->getCode();
+            die('Could not connect to MySQL');
+        }
+    }
+    return $dbc;
+}
---
+++ Programmation/API/Test_login/functions.php
@@ -0,0 +1,29 @@
+<?php
+include "pdo.php";
+
+function GetUserIdByEmail($email){
+  $req = "SELECT `id` FROM `Person` WHERE `email` = :m";
+  $query = database()->prepare($req);
+  $query->bindParam(':m', $email, PDO::PARAM_STR);
+  $query->execute();
+  $matches = $query->fetch(PDO::FETCH_ASSOC);
+  if($matches == null){
+    return null;
+  }else{
+    return $matches;
+  }
+}
+
+function Login($id, $password){
+  $req = "SELECT `first_name`, `last_name` FROM `Person` WHERE `id` = :i AND `password` = :p";
+  $query = database()->prepare($req);
+  $query->bindParam(':i', $id, PDO::PARAM_STR);
+  $query->bindParam(':p', $password, PDO::PARAM_STR);
+  $query->execute();
+  $matches = $query->fetch(PDO::FETCH_ASSOC);
+  if($matches == null){
+    return null;
+  }else{
+    return $matches;
+  }
+}
