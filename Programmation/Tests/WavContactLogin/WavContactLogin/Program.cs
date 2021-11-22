9f32b58621bdccc87dc40a754824c70e3497983e
ConstantinHrrmn <31920335+ConstantinHrrmn@users.noreply.github.com>
2021-11-22 11:28:02 +0100

    Connexion a partir de c#
    Création d'un petit projet c# permettant le login sur le serveur de WavContact
    
    + Hash du mot de passe en c#
    + Lecteur async des réponses par le serveur

---
+++ Programmation/Tests/WavContactLogin/WavContactLogin/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WavContactLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string email = "constantin@waview.ch";
+            string password = "motdepasse";
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Login into WavContact with account : ");
+            Console.WriteLine("Email : " + email);
+            Console.WriteLine("Password : " + password);
+
+            Console.WriteLine("---------------------------");
+
+
+            User u = WavContactPDO.LoginAsync(email, password).Result;
+
+            Console.WriteLine("Login OK : " + u.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programmation/Tests/WavContactLogin/WavContactLogin/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WavContactLogin
+{
+    class User
+    {
+        private string firstname;
+        private string lastname;
+
+        public User(string fn, string ln)
+        {
+            this.firstname = fn;
+            this.lastname = ln;
+        }
+
+        public override string ToString()
+        {
+            return this.firstname + " " + this.lastname;
+        }
+    }
+}
